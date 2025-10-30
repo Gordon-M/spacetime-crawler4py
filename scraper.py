401e364ce223754fa1a54dee3b4e6f33d75820f7
GordonMa <gordom2@uci.edu>
2025-10-30 00:35:04 -0700

    changed ignore_list

--- scraper.py
+++ scraper.py
@@ -12,9 +12,9 @@ def scraper(url, resp):
 
 def parseWords(text):
     #Returns only words. HTML Tags, punctuation, whitespace removed
-    only_words = re.sub(r'[^\w\s]', '', text)
-    remove_tags = re.sub(r'<.*?>', '', only_words)
-    return remove_tags
+    remove_tags = re.sub(r'<.*?>', '', text)
+    only_words = re.sub(r'[^\w\s]', '', remove_tags)
+    return only_words
     
 def ngram_sort(text):
     #returns words grouped into 3-grams
@@ -106,7 +106,9 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
-    ignore_list = ["wics.ics", "ngs.ics", "/doku", "mediamanager.php", "eppstein/pix"]
+    ignore_list = ["ngs.ics", "/doku", "mediamanager.php", "eppstein/pix", "isg.ics.uci.edu/events/",
+    "timeline", "?version=", "?action=diff", "?format=", "?entry_point", "login", "/r.php", "redirect",
+    ]
     calendar_list = ["week", "month", "year", "calendar"]
     try:
         parsed = urlparse(url)
